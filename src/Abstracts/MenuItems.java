56ef385c0e05a1cf95163e32535445d3cbbc472f
DH3155 <127413800+DH3155@users.noreply.github.com>
2023-05-30 08:19:11 +0700

    Delete old src Directory

--- src/Abstracts/MenuItems.java
+++
@@ -1,9 +0,0 @@
-package Abstracts;
-// Abstract for menu item
-public class MenuItems {
-	protected String name;
-	protected int type;
-	// 0 for drinks
-	// 1 for foods
-	protected double price;
-}
--- src/Assets/Room/RoomA.java
+++
@@ -1,5 +0,0 @@
-package Assets.Room;
-
-public class RoomA {
-
-}
