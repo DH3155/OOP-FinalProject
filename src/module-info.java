ce552f84595ce3c913fe8fd71525cb8f64403334
DH3155 <127413800+DH3155@users.noreply.github.com>
2023-06-01 12:25:31 +0700

    Delete module-info.java

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module JavaFxTest {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires java.desktop;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
